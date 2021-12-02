ff3be459c50502ce0ba747313882c709e60e2af8
rivasilver <79878482+rivasilver@users.noreply.github.com>
2021-12-02 17:22:00 +0100

    Fájl kiegészítve

---
+++ masik.js
@@ -0,0 +1,1 @@
+console.log(osszead('2','4'));
---
+++ muveletek.js
@@ -0,0 +1,20 @@
+function osszead(a, b) {
+    a = parseFloat(a);
+    b = parseFloat(b);
+    return a + b;
+}
+function kivon(a, b) {
+    a = parseFloat(a);
+    b = parseFloat(b);
+    return a - b;
+}
+function szoroz(a, b) {
+    a = parseFloat(a);
+    b = parseFloat(b);
+    return a * b;
+}
+function oszt(a, b) {
+    a = parseFloat(a);
+    b = parseFloat(b);
+    return a / b;
+}
